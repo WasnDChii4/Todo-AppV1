f45c2042e3aede509f01d12f201e11d12f1051b4
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-09-28 10:33:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Todo from "./components/Todo";
+
+export default function App() {
+  return (
+    <div className="grid bg-gradient-to-br from-sky-400 to-blue-800 py-4 min-h-screen">
+      <Todo/>
+    </div>
+  )
+}
