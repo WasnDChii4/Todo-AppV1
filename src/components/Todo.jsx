d51ef761cb25668aebd6a6812402d92565f84ea9
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-09-28 10:34:43 +0700

    pembenahan

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
+import {FaPlus, FaTrash, FaCheck} from "react-icons/fa";
 
 export default function Todo() {
+  const [todos, setTodos] = useState([
+    {id: 1, text: 'Learning Todo App', time: '10.00 PM', completed: false}
+  ])
+  const [newTodo, setNewTodo] = useState('')
+
+  // Function for add Todo
+  const addTodo = () => {
+    if(newTodo.trim() == '')
+    return;
+    const currentDate = new Date();
+    const timeString = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    const dateString = currentDate.toLocaleDateString();
+
+    setTodos([
+      {
+        text: newTodo,
+        id: Date.now(),
+        time: `${timeString}, ${dateString}`,
+        completed: false
+      },
+      ...todos
+    ])
+    setNewTodo('')
+  }
+
+  // Delete Todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  // Toggle Complete
+  const completedTodo = (id) => {
+    setTodos(todos.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo))
+  }
 
   return (
-    <div>
+    <div className="bg-white/30 place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xl border border-white/20 shadow-lg shadow-sky-300/50">
 
     </div>
   )
